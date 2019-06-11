c8219cec73e319217adab533884a29b24f22fc84
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-10 21:43:51 -0400

    Finished Closure Challenge 1

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function count () {
+var counter = 0;
 
+    function increment () {
+      return ++counter;
+    }
+   return increment ();
+}
+
+console.log(count());
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
